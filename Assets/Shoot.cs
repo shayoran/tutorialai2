e62ccaaee71ab380dcdf5ee494fbeb1b8927d6a2
shayoran <104832902+shayoran@users.noreply.github.com>
2022-11-03 23:12:55 +0000

    FranciscoGomesa48253-Calculatingtrajectories part1

--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -6,6 +6,9 @@ public class Shoot : MonoBehaviour
 {
     public GameObject shellPrefab;
     public GameObject shellSpawnPos;
+    public GameObject target;
+    public GameObject parent;
+    float speed = 15;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +24,34 @@ public class Shoot : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space))
         Fire();
+
+        Vector3 direction = (target.transform.position - parent.transform.position).normalized;
+        Quaternion lookRotation = Quaternion lookRotation(new Vector3(direction.x, 0 , direction.z))
+        parent.transform.rotation = Quaternion.slerp(parent.transform.rotation, lookRotation, Time.deltaTime * turnspeed);
+    }
+
+    float? CalculateAngle(bool low)
+    {
+        Vector3 targetDir = target.transform.position = this.transform.position;
+        float y =targetDir.y;
+        targetDir.y = 0f;
+        float x = targetDir.magnitude;
+        float.gravity = 9.81f;
+        float sSqr = speed * speed;
+        float underTheSqrRoot = (sSqr * sSqr) - gravity * (gravity * x * x + 2 * y * sSqr);
+
+        if(underTheSqrRoot >= 0f)
+        {
+            float root = Mathf.Sqrt(underTheSqrRoot);
+            float highAngle = sSqr + root;
+            float lowAngle = sSqr - root;
+
+            if(low)
+            return (Mathf.Atan2(lowAngle, gravity * x) * Mathf.Rad2Deg);
+            else
+            return (Mathf.Atan2(highAngle, gravity * x) * Mathf.Rad2Deg);
+        }
+        else
+        return null;
     }
 }

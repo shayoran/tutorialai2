3dbe9b92de05af2c8b1bfbdd52a0d62f262d9bc8
shayoran <104832902+shayoran@users.noreply.github.com>
2022-11-05 17:56:55 +0000

    FranciscoGomesa48253-calculating trajectories final

--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class Shell : MonoBehaviour
 {
     public GameObject explosion;
-    float mass = 10;
-    float force = 200;
-    float acceleration;
-    float gravity = -9.8f;
-    float gAccel;
-    float speedZ;
-    float speedY;
-    
+    Rigidbody rb;
+    //float mass = 10;
+    //float force = 200;
+    //float acceleration;
+    //float gravity=-9.8f;
+    //float gAccel;
+    //float speedZ;
+    //float speedY;
 
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "tank")
         {
             GameObject exp = Instantiate(explosion, this.transform.position, Quaternion.identity);
-            Destroy(exp, 0.5f);
+            Destroy(exp, 0.2f);
             Destroy(this.gameObject);
         }
     }
@@ -27,19 +27,19 @@ public class Shell : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        /*acceleration = force / mass;
-        speedZ += acceleration * Time.deltaTime;
-        gAccel = gravity / mass;
-        speedY += gAccel * Time.deltaTime;
-        this.transform.Translate(0, speedY, speedZ);*/
-
-        force = 0;
+        //acceleration = force/mass;
+        //speedZ += acceleration * Time.deltaTime;
+        //gAccel = gravity / mass;
+        //speedY += gAccel * Time.deltaTime;
+        //this.transform.Translate(0, speedY, speedZ);
+        //force = 0;
         this.transform.forward = rb.velocity;
     }
 }
+
--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -8,44 +8,49 @@ public class Shoot : MonoBehaviour
     public GameObject shellSpawnPos;
     public GameObject target;
     public GameObject parent;
-    float speed = 15;
-    float turnspeed = 2;
+
+    float turnSpeed=2;
+    float speed = 30;
     bool canShoot = true;
     // Start is called before the first frame update
     void Start()
     {
         
     }
+
     void CanShootAgain()
     {
         canShoot = true;
     }
     void Fire()
     {
-        if(canShoot)
+        if (canShoot)
         {
             GameObject shell = Instantiate(shellPrefab, shellSpawnPos.transform.position, shellSpawnPos.transform.rotation);
-            shell.GetComponent<RigidBody>().velocity = speed * this.transform.forward;
+            shell.GetComponent<Rigidbody>().velocity = speed * this.transform.forward;
             canShoot = false;
-            Invoke = ("CanShootAgain",0.2f);
+            Invoke("CanShootAgain", 0.5f);
         }
     }
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        Fire();
+        
 
         Vector3 direction = (target.transform.position - parent.transform.position).normalized;
-        Quaternion lookRotation = Quaternion lookRotation(new Vector3(direction.x, 0 , direction.z))
-        parent.transform.rotation = Quaternion.slerp(parent.transform.rotation, lookRotation, Time.deltaTime * turnspeed);
-
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        parent.transform.rotation = Quaternion.Slerp(parent.transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
         float? angle = RotateTurret();
+
+        if(angle!=null && Vector3.Angle(direction, parent.transform.forward) < 10)
+        {
+            Fire();
+        }
     }
 
-    float?RotateTurret()
+    float? RotateTurret()
     {
-        float? CalculateAngle(false);
+        float? angle = CalculateAngle(true); // "true" escolhe o low angle, "false" escolhe o high angle
         if (angle != null)
         {
             this.transform.localEulerAngles = new Vector3(360f - (float)angle, 0f, 0f);
@@ -53,28 +58,35 @@ public class Shoot : MonoBehaviour
         return angle;
     }
 
-    float? CalculateAngle(bool low)
+    float? CalculateAngle(bool low) // "float?" -> retorn float ou null
     {
-        Vector3 targetDir = target.transform.position = this.transform.position;
-        float y =targetDir.y;
+        Vector3 targetDir = target.transform.position - this.transform.position;
+        float y = targetDir.y;
         targetDir.y = 0f;
         float x = targetDir.magnitude;
-        float.gravity = 9.81f;
+        float gravity = 9.81f;
         float sSqr = speed * speed;
         float underTheSqrRoot = (sSqr * sSqr) - gravity * (gravity * x * x + 2 * y * sSqr);
 
-        if(underTheSqrRoot >= 0f)
+        if (underTheSqrRoot>=0f)
         {
             float root = Mathf.Sqrt(underTheSqrRoot);
             float highAngle = sSqr + root;
             float lowAngle = sSqr - root;
 
-            if(low)
-            return (Mathf.Atan2(lowAngle, gravity * x) * Mathf.Rad2Deg);
+            if (low)
+            {
+                return (Mathf.Atan2(lowAngle, gravity + x) * Mathf.Rad2Deg);
+            }
             else
-            return (Mathf.Atan2(highAngle, gravity * x) * Mathf.Rad2Deg);
+            {
+                return (Mathf.Atan2(highAngle, gravity + x) * Mathf.Rad2Deg);
+            }
         }
         else
-        return null;
+        {
+            return null;
+        }
     }
 }
+

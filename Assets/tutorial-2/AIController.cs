e31ca57c4d7d15649fc5468f5241f9f0b612cec7
shayoran <104832902+shayoran@users.noreply.github.com>
2022-11-10 14:57:46 +0000

    FrancicscoGomesa48253-CrowdSimulation part1

--- Assets/tutorial-2/AIController.cs
+++ Assets/tutorial-2/AIController.cs
@@ -1,22 +1,20 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class AIController : MonoBehaviour
-{
-    public GameObject goal;
-    UnityEngine.AI.NavMeshAgent agent;
-    // Start is called before the first frame update
-    void Start()
-    {
-        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
-        agent.SetDestination(goal.transform.position);
-    }
+public class AIController : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // Agents destination
+    public GameObject goal;
+    // Get the prefab
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start() {
+        // Access the agents NavMesh
+        agent = this.GetComponent<NavMeshAgent>();
+        // Instruct the agent where it has to go
+        agent.SetDestination(goal.transform.position);
     }
 }

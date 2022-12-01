c557f2b0d9cdaf00cce584b8f344d25f817b86f8
shayoran <104832902+shayoran@users.noreply.github.com>
2022-12-01 22:58:03 +0000

    FrancicscoGomesa48253-creating a citycrowd part1B

---
+++ Assets/tutorial-2/Creating city crowd/AIControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class AIControl : MonoBehaviour {
+
+    GameObject[] goalLocations;
+    UnityEngine.AI.NavMeshAgent agent;
+    Animator anim;
+
+
+    // Use this for initialization
+    void Start() {
+        goalLocations = GameObject.FindGameObjectsWithTag("goal");
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.SetDestination(goalLocations [Random.Range(0,goalLocations.Length)].transform.position);
+        anim = this.GetComponent<Animator>();
+        anim.SetTrigger("isWalking");
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (agent.remainingDistance < 1) {
+            agent.SetDestination(goalLocations[Random.Range(0,goalLocations.Length)].transform.position);
+        }
+    }
+}

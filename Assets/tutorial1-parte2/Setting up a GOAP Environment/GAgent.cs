272600a4ed3fa61bfcc526a649ccfcdcfe78dfb2
shayoran <104832902+shayoran@users.noreply.github.com>
2022-12-04 21:32:57 +0000

    FrancicscoGomesa48253-Waypoins§GraphsAction/Agents

--- Assets/tutorial1-parte2/Setting up a GOAP Environment/GAgent.cs
+++ Assets/tutorial1-parte2/Setting up a GOAP Environment/GAgent.cs
@@ -1,18 +1,45 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GAgents : MonoBehaviour
-{
+public class SubGoal {
+
+    // Dictionary to store our goals
+    public Dictionary<string, int> sGoals;
+    // Bool to store if goal should be removed
+    public bool remove;
+
+    // Constructor
+    public SubGoal(string s, int i, bool r) {
+
+        sGoals = new Dictionary<string, int>();
+        sGoals.Add(s, i);
+        remove = r;
+    }
+}
+
+public class GAgent : MonoBehaviour {
+
+    public List<GAction> actions = new List<GAction>();
+    public Dictionary<SubGoal, int> goals = new Dictionary<SubGoal, int>();
+    public WorldStates beliefs = new WorldStates();
+
+    GPlanner planner;
+    Queue<GAction> actionQueue;
+    public GAction currentAvtion;
+    SubGoal currentGoal;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
+        GAction[] acts = this.GetComponents<GAction>();
+        foreach (GAction a in acts) {
+
+            actions.Add(a);
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void LateUpdate() {
+
     }
 }

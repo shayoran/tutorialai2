8efded17021a0d8709e3f367c7cc395653d471b1
shayoran <104832902+shayoran@users.noreply.github.com>
2022-12-05 23:26:52 +0000

    FrancicscoGomesa48253-Goal Driven Taking a Break

--- Assets/tutorial1-parte2/Setting up a GOAP Environment/Nurse.cs
+++ Assets/tutorial1-parte2/Setting up a GOAP Environment/Nurse.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace part2 {
-public class Nurse : GAgent
-{
-    // Start is called before the first frame update
-    new void Start()
-    {
+public class Nurse : GAgent {
+
+    new void Start() {
+
+        // Call base Start method
         base.Start();
+        // Set goal so that it can't be removed so the nurse can repeat this action
         SubGoal s1 = new SubGoal("treatPatient", 1, false);
-        goals.Add(s1, 3); 
+        goals.Add(s1, 3);
+
+        // Resting goal
+        SubGoal s2 = new SubGoal("rested", 1, false);
+        goals.Add(s2, 1);
+
+        // Call the GetTired() method for the first time
+        Invoke("GetTired", Random.Range(10.0f, 20.0f));
+    }
+
+    void GetTired() {
+
+        beliefs.ModifyState("exhausted", 0);
+        //call the get tired method over and over at random times to make the nurse
+        //get tired again
+        Invoke("GetTired", Random.Range(0.0f, 20.0f));
     }
 
 }
---
+++ Assets/tutorial1-parte2/Setting up a GOAP Environment/Rest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace part2 {
+public class Rest : GAction {
+    public override bool PrePerform() {
+
+        return true;
+    }
+
+    public override bool PostPerform() {
+
+        //the agent will no longer believe they need a rest
+        beliefs.RemoveState("exhausted");
+        return true;
+    }
+}
+}
